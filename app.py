195be49f78bd774a7ccf82050be7f367b6452436
nicolasvodoz <n.vodoz@gmail.com>
2018-11-10 01:55:13 +0100

    template ok

--- app.py
+++ app.py
@@ -2,12 +2,33 @@ from flask import Flask, render_template, flash, request, session, jsonify
 
 
 app = Flask(__name__)
+app.secret_key = 'IsYRsz62EKhfWRKHEP2dyPIKGx55CD3G'
 
 
 @app.route('/')
 def consumer():
-    return render_template('consumer.html')
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('consumer.html')
+
+
+@app.route('/login/', methods=['POST'])
+def login():
+    password = '1234'
+    if request.form['password'] == password and request.form['username'] == 'config':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return consumer()
+
+
+@app.route("/logout/")
+def logout():
+    print('yeah')
+    session['logged_in'] = False
+    return consumer()
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
