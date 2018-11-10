6e014f1d9e7c70976edfe5ebf873e3ce5caae91f
nicolasvodoz <n.vodoz@gmail.com>
2018-11-10 02:15:32 +0100

    login ready

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.secret_key = 'IsYRsz62EKhfWRKHEP2dyPIKGx55CD3G'
 
 
-@app.route('/')
+@app.route('/consumer/')
 def consumer():
     if not session.get('logged_in'):
         return render_template('login.html')
@@ -13,22 +13,40 @@ def consumer():
         return render_template('consumer.html')
 
 
+@app.route('/admin/')
+def admin_view():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('admin.html')
+
+
 @app.route('/login/', methods=['POST'])
 def login():
+    admin = 'frank.grassi@monthey.ch'
+    consumer_user = 'consumer@mail.com'
+
     password = '1234'
-    if request.form['password'] == password and request.form['username'] == 'config':
+
+    if request.form['password'] == password and request.form['username'] == consumer_user:
         session['logged_in'] = True
+
+        return consumer()
+
+    elif request.form['password'] == password and request.form['username'] == admin:
+        session['logged_in'] = True
+
+        return admin_view()
+
     else:
         flash('wrong password!')
-    return consumer()
 
 
 @app.route("/logout/")
 def logout():
-    print('yeah')
     session['logged_in'] = False
     return consumer()
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=True)
